5786de352711596961fedc41a91b9676d41ce7bf
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-08-17 02:04:33 +0530

    Initial Deployment!

---
+++ customDataset.py
@@ -0,0 +1,24 @@
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch.utils.data import Dataset
+from PIL import Image
+
+class CustomDataset(Dataset):
+    def __init__(self, pairs, transform=None):
+        self.pairs = pairs
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, idx):
+        img_path, mask_path = self.pairs[idx]
+
+        image = Image.open(img_path).convert('RGB')
+        mask = Image.open(mask_path).convert('L')
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image, mask
---
+++ test.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+from model import UNet 
+from customDataset import CustomDataset  
+
+
+model_path = 'best_model.pth'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = UNet(n_class=1) 
+checkpoint = torch.load(model_path, map_location=device)
+model.load_state_dict(checkpoint['model_state_dict'])
+model.to(device)
+model.eval()
+
+
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+])
+
+def predict_single_image(img_path, model, transform, device):
+    image = Image.open(img_path).convert('RGB')
+    image = transform(image).unsqueeze(0).to(device)
+    
+    with torch.no_grad():
+        output = model(image)
+        output = torch.sigmoid(output)
+        output = output.squeeze().cpu().numpy()
+    
+    return output
+
+test_image_path = 'random.jpg' 
+predicted_mask = predict_single_image(test_image_path, model, transform, device)
+
+# Visualize the result
+original_image = Image.open(test_image_path)
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(original_image)
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(predicted_mask, cmap='gray')
+plt.title('Predicted Mask')
+plt.axis('off')
+
+plt.show()
+
